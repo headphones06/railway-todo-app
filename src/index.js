a826147d9d2d5335511a08b2a1d1237c1f471907
headphones06 <headphones.CG@gmail.com>
2022-08-07 23:37:18 +0900

    面談前コミット

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReatDOM from 'react-dom/client';
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { CookiesProvider } from "react-cookie";
-import { Provider } from "react-redux";
-import { store } from "./store"
+import React from 'react'
+import ReatDOM from 'react-dom/client'
+import './index.css'
+import { CookiesProvider } from 'react-cookie'
+import { Provider } from 'react-redux'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { store } from './store'
 
 ReatDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>,
-);
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { NotFound } from "../pages/NotFound";
-import { SignIn } from "../pages/SignIn";
-import { NewTask } from "../pages/NewTask";
-import { NewList } from "../pages/NewList";
-import { EditTask } from "../pages/EditTask";
-import { SignUp } from "../pages/SignUp";
-import { EditList } from "../pages/EditList";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Home } from '../pages/Home'
+import { NotFound } from '../pages/NotFound'
+import { SignIn } from '../pages/SignIn'
+import { NewTask } from '../pages/NewTask'
+import { NewList } from '../pages/NewList'
+import { EditTask } from '../pages/EditTask'
+import { SignUp } from '../pages/SignUp'
+import { EditList } from '../pages/EditList'
 
-export const Router = () => {
+export function Router() {
   const auth = useSelector((state) => state.auth.isSignIn)
-  
+
   return (
     <BrowserRouter>
       <Routes>
@@ -26,8 +26,9 @@ export const Router = () => {
             <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
             <Route path="/lists/:listId/edit" element={<EditList />} />
           </>
-        ) : <Route path="*" element={<Navigate to="/signin" />} />
-        }
+        ) : (
+          <Route path="*" element={<Navigate to="/signin" />} />
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
